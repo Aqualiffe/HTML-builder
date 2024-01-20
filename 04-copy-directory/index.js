fbe5ca37c688de55ca3e530712a84071f3efbf0a
Aqualiffe <evgeniia.er@gmail.com>
2024-01-20 15:54:22 +0300

    feat: add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,14 +1,36 @@
 const fs = require('fs');
 const path = require('path');
+const pathFolder = path.join(__dirname, 'files');
+const pathCopyFolder = path.join(__dirname, 'files-copy');
 
-async function copyDir() {
-  fs.mkdir(path.join(__dirname, 'files-copy'),
-    (err) => {
-      if (err) {
-        return console.error(err);
-    }
-    }
-  );
+function copyDir() {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+      if (err) throw err;
+  });
+  fs.readdir(pathCopyFolder, function(err, files) {
+    if (err) throw err;
+    files.forEach(file => {
+      const oldPathFile = path.join(pathCopyFolder, file);
+      console.log('старые файлы: ' + oldPathFile);
+      fs.unlink(oldPathFile, (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+
+  fs.readdir(pathFolder, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+      if (file.isFile()) {
+        const pathFile = path.join(pathFolder, file.name);
+        const newPathFile = path.join(pathCopyFolder, file.name)
+        fs.copyFile(pathFile, newPathFile, err => {
+          if (err) throw err;
+          console.log('Новые файлы: ' + newPathFile);
+        })
+      }
+    });
+  });
 }
 
 copyDir();
