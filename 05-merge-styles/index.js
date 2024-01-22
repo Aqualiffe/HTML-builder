4e668f124f59881b1a492d12a52b6499b6ddc177
Aqualiffe <evgeniia.er@gmail.com>
2024-01-22 12:09:54 +0300

    feat: add solution 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,13 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 const pathStyles = path.join(__dirname, 'styles');
+const pathOutput = path.join(__dirname, 'project-dist', 'bundle.css');
+const writeStream = fs.createWriteStream(pathOutput, 'utf-8');
+
+console.log('Файл создан');
 
 fs.readdir(pathStyles, {withFileTypes: true}, (err, files) => {
   if (err) throw err;
   files.forEach((file) => {
-    const filePath = path.join(pathStyles, file.name)
+    const filePath = path.join(pathStyles, file.name);
+    const readStream = fs.createReadStream(filePath, 'utf-8');
     if (file.isFile() && path.extname(filePath) === '.css') {
-      console.log(filePath);
+      readStream.pipe(writeStream);
     }
   });
 });
